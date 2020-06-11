9b769c8af0ec01d97672aae647a7c0703a91ad20
Nick Bennett <nick@nickbenn.com>
2020-06-11 11:02:28 -0600

    Building out Game class.

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/model/DieState.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/model/DieState.java
@@ -3,8 +3,17 @@ package edu.cnm.deepdive.dicesolitaire.model;
 public enum DieState {
 
   AVAILABLE,
-  UNAVAILABLE,
+  UNAVAILABLE {
+    @Override
+    public boolean isSelectable() {
+      return false;
+    }
+  },
   SCRATCH,
   PAIR;
+
+  public boolean isSelectable() {
+    return true;
+  }
 
 }
